069d622a49c0ab50b7f6d9ae2e13ab21d16f015c
Sean_Cheetham <skc010@bucknell.edu>
2014-09-12 20:54:27 -0400

    Finished histogram

--- functions.rb
+++ functions.rb
@@ -1,4 +1,21 @@
 def reverse(an_array)
-	length = an_array.length
+	an_array.reverse
+end
+
+def histogram(a_string)
+	hist = {}
+	a_string.split('').each{ |letter|
+		letter = letter.downcase
+
+		if !hist.key?(letter)
+
+			hist[letter] = 1
+		else
+			hist[letter] += 1
+		end
+
+	}
+
+	hist
 
 end
